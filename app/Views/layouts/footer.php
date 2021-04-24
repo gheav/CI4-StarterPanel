85418255668161a3b5b328a9f96304230fb37a45
Gilang Heavy <38318210+gheav@users.noreply.github.com>
2021-04-24 19:08:10 +0700

    Layouting Section

---
+++ app/Views/layouts/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <div class="environment">
+        <p>Page rendered in {elapsed_time} seconds</p>
+        <p>Environment: <?= ENVIRONMENT ?></p>
+    </div>
+    <div class="copyrights">
+        <p>&copy; <?= date('Y') ?> CodeIgniter Foundation. CodeIgniter is open source project released under the MIT
+            open source licence.</p>
+    </div>
+</footer>
