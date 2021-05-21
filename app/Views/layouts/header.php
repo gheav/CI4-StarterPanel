12c581cfec880104244efcff9f90eedbc783d798
Gilang Heavy <38318210+gheav@users.noreply.github.com>
2021-05-22 00:16:52 +0700

    Update header.php
    get name

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -143,7 +143,7 @@
                 </a>
 
                 <a class="nav-link dropdown-toggle d-none d-sm-inline-block" href="#" data-bs-toggle="dropdown">
-                    <img src="<?= base_url('assets/img/avatars/avatar.jpg') ?>" class="avatar img-fluid rounded me-1" alt="Charles Hall" /> <span class="text-dark">Charles Hall</span>
+                    <img src="<?= base_url('assets/img/avatars/avatar.jpg') ?>" class="avatar img-fluid rounded me-1" alt="<?= $user['fullname']; ?>" /> <span class="text-dark"><?= $user['fullname']; ?></span>
                 </a>
                 <div class="dropdown-menu dropdown-menu-end">
                     <a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
