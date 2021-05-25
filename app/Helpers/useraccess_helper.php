8409d62bbfdb020c7b85051e66bfee267bc2f0ca
Gilang Heavy <38318210+gheav@users.noreply.github.com>
2021-05-25 18:36:25 +0700

    create function checking menu category access

--- app/Helpers/useraccess_helper.php
+++ app/Helpers/useraccess_helper.php
@@ -1,12 +1,17 @@
 <?php
+function check_menuCategory_access($role_id, $menu_category_id)
+{
+    $db                     = \Config\Database::connect();
+    $accessMenuCategory     = $db->table('user_access')->where(['role_id' => $role_id, 'menu_category_id' => $menu_category_id])->countAllResults();
+    if ($accessMenuCategory > 0) {
+        return "checked";
+    }
+}
 function check_menu_access($role_id, $menu_id)
 {
-    $db     = \Config\Database::connect();
-    $result = $db->table('user_access')->where([
-        'role_id' => $role_id,
-        'menu_id' => $menu_id
-    ])->countAllResults();
-    if ($result > 0) {
+    $db                 = \Config\Database::connect();
+    $accessMenu         = $db->table('user_access')->where(['role_id' => $role_id, 'menu_id' => $menu_id])->countAllResults();
+    if ($accessMenu > 0) {
         return "checked";
     }
 }
